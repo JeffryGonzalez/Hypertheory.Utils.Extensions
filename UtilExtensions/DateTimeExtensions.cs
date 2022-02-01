3d798a652055bc9000398424b19369776f1c4be2
Jeff Gonzalez <jeff@hypertheory.com>
2022-02-01 18:56:58 -0500

    ready to push

---
+++ UtilExtensions/DateTimeExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace UtilExtensions;
+
+public static class DateTimeExtensions
+{
+    public static void Deconstruct(this DateTime d, out int month, out int day, out int year)
+    {
+        day = d.Day;
+        month = d.Month;
+        year = d.Year;
+    }
+}
---
+++ UtilExtensionsTests/UnitTest1.cs
@@ -0,0 +1,30 @@
+using System;
+using Xunit;
+using UtilExtensions;
+namespace UtilExtensionsTests;
+
+public class DateExtensionTests
+{
+    [Fact]
+    public void CanDeconstructDates()
+    {
+        var birthDay = new DateTime(1969, 4, 20);
+
+        var (month, day, year) = birthDay;
+        
+        Assert.Equal(4, month);
+        Assert.Equal(20, day);
+        Assert.Equal(1969, year);
+
+    }
+
+    [Fact]
+    public void DeconstructPartialDate()
+    {
+        var birthDay = new DateTime(1969, 4, 20);
+
+        var( _, _, year) = birthDay;
+        
+        Assert.Equal(1969, year);
+    }
+}
